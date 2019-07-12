44a5118b17c0a3d4a927b06a089164bfc2c1388e
aoponcedeleon <aoponcedeleon@up.edu.ph>
2019-07-12 14:20:57 +0800

    Completed Contact filter form and state implementation.

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -71,19 +71,26 @@ const ContactState = props => {
   };
 
   //   Filter Contacts
-
+  const filterContacts = text => {
+    dispatch({ type: FILTER_CONTACTS, payload: text });
+  };
   //   Clear Filter
-
+  const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER });
+  };
   return (
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
         current: state.current,
+        filtered: state.filtered,
         addContact,
         deleteContact,
         setCurrent,
         clearCurrent,
-        updateContact
+        updateContact,
+        filterContacts,
+        clearFilter
       }}
     >
       {props.children}
